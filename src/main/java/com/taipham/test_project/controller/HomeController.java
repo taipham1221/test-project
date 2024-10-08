9cd73f86d782946a0cbc0536a3e64ef466b1a8e9
taipham1221 <taipham8302@gmail.com>
2024-10-08 09:13:32 +0700

    change

--- src/main/java/com/taipham/test_project/controller/HomeController.java
+++ src/main/java/com/taipham/test_project/controller/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
     
     @GetMapping
     public String helloWord() {
-        return "Hello, Chi Oanh!!!!!!!!!!";
+        return "Hello, Chi Oanh!!!!!!!!";
     }
 
     @GetMapping("/healthz")
